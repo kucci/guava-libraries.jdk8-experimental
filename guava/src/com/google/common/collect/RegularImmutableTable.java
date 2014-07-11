bde726cef2ed498ed82850dc0b6517ca14dea99b
Louis Wasserman <lowasser@google.com>
2014-07-11 14:10:26 -0700

    Simplify comparator usage in RegularImmutableTable ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=70978800

--- guava/src/com/google/common/collect/RegularImmutableTable.java
+++ guava/src/com/google/common/collect/RegularImmutableTable.java
@@ -15,6 +15,7 @@
 package com.google.common.collect;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static java.util.Comparator.comparing;
 
 import com.google.common.annotations.GwtCompatible;
 
@@ -112,26 +113,22 @@ abstract class RegularImmutableTable<R, C, V> extends ImmutableTable<R, C, V> {
       @Nullable final Comparator<? super R> rowComparator,
       @Nullable final Comparator<? super C> columnComparator) {
     checkNotNull(cells);
-    if (rowComparator != null || columnComparator != null) {
-      /*
-       * This sorting logic leads to a cellSet() ordering that may not be expected and that isn't
-       * documented in the Javadoc. If a row Comparator is provided, cellSet() iterates across the
-       * columns in the first row, the columns in the second row, etc. If a column Comparator is
-       * provided but a row Comparator isn't, cellSet() iterates across the rows in the first
-       * column, the rows in the second column, etc.
-       */
-      Comparator<Cell<R, C, V>> comparator = new Comparator<Cell<R, C, V>>() {
-        @Override public int compare(Cell<R, C, V> cell1, Cell<R, C, V> cell2) {
-          int rowCompare = (rowComparator == null) ? 0
-            : rowComparator.compare(cell1.getRowKey(), cell2.getRowKey());
-          if (rowCompare != 0) {
-            return rowCompare;
-          }
-          return (columnComparator == null) ? 0
-              : columnComparator.compare(cell1.getColumnKey(), cell2.getColumnKey());
-        }
-      };
-      Collections.sort(cells, comparator);
+    /*
+     * This sorting logic leads to a cellSet() ordering that may not be expected and that isn't
+     * documented in the Javadoc. If a row Comparator is provided, cellSet() iterates across the
+     * columns in the first row, the columns in the second row, etc. If a column Comparator is
+     * provided but a row Comparator isn't, cellSet() iterates across the rows in the first
+     * column, the rows in the second column, etc.
+     */
+    if (rowComparator != null) {
+      if (columnComparator != null) {
+        cells.sort(comparing(Cell<R, C, V>::getRowKey, rowComparator)
+            .thenComparing(Cell<R, C, V>::getColumnKey, columnComparator));
+      } else {
+        cells.sort(comparing(Cell<R, C, V>::getRowKey, rowComparator));
+      }
+    } else if (columnComparator != null) {
+      cells.sort(comparing(Cell<R, C, V>::getColumnKey, columnComparator));
     }
     return forCellsInternal(cells, rowComparator, columnComparator);
   }
@@ -159,14 +156,12 @@ abstract class RegularImmutableTable<R, C, V> extends ImmutableTable<R, C, V> {
 
     ImmutableSet<R> rowSpace = rowSpaceBuilder.build();
     if (rowComparator != null) {
-      List<R> rowList = Lists.newArrayList(rowSpace);
-      Collections.sort(rowList, rowComparator);
+      List<R> rowList = Ordering.from(rowComparator).immutableSortedCopy(rowSpace);
       rowSpace = ImmutableSet.copyOf(rowList);
     }
     ImmutableSet<C> columnSpace = columnSpaceBuilder.build();
     if (columnComparator != null) {
-      List<C> columnList = Lists.newArrayList(columnSpace);
-      Collections.sort(columnList, columnComparator);
+      List<C> columnList = Ordering.from(columnComparator).immutableSortedCopy(columnSpace);
       columnSpace = ImmutableSet.copyOf(columnList);
     }
 
